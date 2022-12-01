0e6da1a5f132f0a9bda2d211eadfe5dbddc14cfe
Bruno Gabriel <brunosdbr@hotmail.com>
2022-12-01 16:55:34 -0300

    Atualização formulário

--- ideiasdeprojetos/formulariodecadastro/email.php
+++ ideiasdeprojetos/formulariodecadastro/email.php
@@ -4,6 +4,7 @@ if(isset($_POST['email']) && !empty($_POST['email'])){
 
 $nome = addslashes($_POST['name']);
 $email = addslashes($_POST['email']);
+$fone = addslashes($_POST['fone']);
 $mensagem = addslashes($_POST['message']);
 
 
@@ -11,13 +12,14 @@ $to = "brunosdbr@hotmail.com";
 $subject = "Contato - Formulario teste";
 $body = "Nome: ".$nome. "\r\n".
         "Email: ".$email."\r\n".
+        "Fone: ".$telefone."\r\n".
         "Mensagem: ".$mensagem;
 $header = "From:brunosdbr@gmail.com"."\r\n".
             "Reply-To:".$email."\e\n".
             "X=Mailer:PHP/".phpversion();
 
 if (mail($to,$subject,$body,$header)){
-
+    
     echo("Email enviado com sucesso");
 }else{
     echo("O Email não pode ser enviado");
